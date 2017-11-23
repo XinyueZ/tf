848030b0d3f9a806fd79240a419dc2a547564970
Chris Zhao <xinyue.zhao@immonet.de>
2017-11-23 15:39:32 +0100

    Added sample to tf_beginner.py

--- tf_beginner.py
+++ tf_beginner.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 import tensorflow as tf
 
 # What is loss-function, what is training?
@@ -11,12 +12,10 @@ import tensorflow as tf
 # we all know it at university.
 # For more others my suggestion is to find some special books :)
 
-# OK, return to TF, in TF it out loss-function looks like
-# loss = tf.reduce_sum(tf.square(linear_model - y)) / 8
-# If you don't understand how, what, why it works, I clear that in codes below.
+# If you don't understand how, what, why all works, I clear that in codes below.
 
 # We have some data (x,y) |= {(22, 18)，(25, 15)，(28, 12)，(30, 10)......}
-# I guss they're coming from a linear-model(Data scientist can give more suggestions.)
+# I guss they're coming from a linear-model(Data-scientist can give more suggestions.)
 # Well, our target is the typical model f(x) = a * x + b which we want to train.
 # With TF and training we'd find (a, b) in the model which can help us with linear-mode
 # to estimate more and more (x, y)s widely from what we've already had.
@@ -42,8 +41,48 @@ import tensorflow as tf
 # Generally you should break training with expectation like 0 or 0.0000000000001.
 # Sometimes you define times of training, i.e training only 50000 times on your computer.
 
+# Example:
+#
+# These are data I have from a data-scientist,
+# he said it might be from a linear-model like y = a * x + b.
+x_train = [22, 25, 28, 30]
+y_train = [18, 15, 12, 10]
 
+# Our model will find out the pair (a, b).
+# Give first estimation (-1, 50).
+a = tf.Variable([-1.], tf.float32)
+b = tf.Variable([50.], tf.float32)
 
+# Placeholder for all input-pairs (x, y).
+x = tf.placeholder(tf.float32)
+y = tf.placeholder(tf.float32)
 
+# Our expected model
+activation = tf.add(tf.multiply(a, x), b)  # f(x) = a * x + b.
 
+# Our super-start, the loss-function, my expection is 0, that means:
+# the final (a, b) produce a loss(a, b) === 0 with all (x, y)s.
+# loss(a,b) = 1/(2 * n) * Σ|activation - input_y|^2
+loss = tf.reduce_sum(tf.square(activation - y)) / (2 * 4)  # We have 4 data in hand
 
+# Important: TF can inject x, y into activation and loss automatically.
+
+# Training start
+
+# 1. Ready, it is rule, syntax of TF, don't ask why, it must be done for every training.
+init = tf.initialize_all_variables()
+session = tf.Session()
+session.run(init)
+
+# 2. Inject all data to their positions, and the loss-function will calculated by injections.
+# That's the 1st training.
+print("loss: %s" % (session.run(loss, {x: x_train, y: y_train})))  # run() do all injections.
+
+# 3. We give a new pair of (a, b).
+# That's the 2nd training.
+session.run([tf.assign(a, [-1.]),
+             tf.assign(b, [40.])])
+print("loss: %s" % (session.run(loss, {x: x_train, y: y_train})))  # run() do all injections.
+
+# You see, the 2nd training output is 0.0 !
+# Fine!, the target pair of (a, b) is (-1, 40)
