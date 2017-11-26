674b77f218cd799200a8b2e8692c5528845d441f
Chris Zhao <xinyue.zhao@immonet.de>
2017-11-26 13:38:45 +0100

    Updated comments ann sample

--- tf_beginner_ann_helloworld.py
+++ tf_beginner_ann_helloworld.py
@@ -38,6 +38,25 @@ import numpy as np
 # The random is being generated with the standard-deviation.
 # Don't care about deviation, you can do all what you can to generate this array(matrix).
 
+# The whole process will be like this:
+
+#         Input                                           weight-0                                        step 1 finished                                  weight-1                     Done
+#
+#
+# [                   ]                          [                   ]                             [                   ]                            [                   ]        [                   ]
+#     [34, 54, 57],
+#                                                     34, 55, 45 ....                                       55                                               77                          45
+#     [34, 54, 57],
+#
+#     [34, 54, 57],       Do Multiplication           35, 46, 35 ....        get layer 0 > layer 1          35                Do Multiplication              67              =>          -46
+#
+#     [34, 54, 57],
+#
+#     [34, 54, 57],                                   55, 45, 45 ....                                       45                                               56                          -5
+#
+#     ....  ....                                                                                           ....  ....                                       ....  ....                 ....  ....
+# [                   ]                          [                   ]                              [                   ]                           [                   ]        [                   ]
+
 # As master of this factory I want to checkout fist N couple of components.
 N = 1000
 
@@ -48,9 +67,9 @@ Weights_1 = tf.constant(np.random.normal(loc=1.0, scale=5.0, size=(3, N)))
 Weights_2 = tf.constant(np.random.normal(loc=1.0, scale=5.0, size=(N, 1)))
 
 # Make an artificial neural network(ANN) with
-# layer 0: one input ground
-# layer 1: one hide layer
-# layer 2: one output
+# layer 0:   input layer
+# layer 1:   hide layer
+# layer 2:   output layer
 # Rule:
 #       when output is > 0 ->>> it is a good component(cube).
 #       otherwise ->>> it is out of quality.
