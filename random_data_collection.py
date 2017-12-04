3cd74081a84a312fce269e7c0137cef83ee1abdc
Chris Zhao <xinyue.zhao@immonet.de>
2017-12-04 23:47:25 +0100

    Just add sample to generate nx2 and nx1 collection

---
+++ random_data_collection.py
@@ -0,0 +1,10 @@
+from numpy.random import RandomState
+import tensorflow as tf
+
+rdm = RandomState(1)
+dataset_size = 128
+X = rdm.rand(dataset_size, 2)
+Y = [[int(x1 + x2 < 1)] for (x1, x2) in X]
+
+print X
+print Y
